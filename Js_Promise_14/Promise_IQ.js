02e0545bd101f0186d85ef3075eab79d6fde3a0c
Shital Gawande <shitalgawande01@gmail.com>
2026-04-02 14:59:13 +0530

    Code for Promise

--- Js_Promise_14/Promise_IQ.js
+++ Js_Promise_14/Promise_IQ.js
@@ -93,3 +93,14 @@ Promise.allSettled([
 }).catch((err)=>{
     console.log("Error: ", err);
 });
+
+Promise.allSettled([
+    Promise.resolve("API 200"),
+    Promise.reject("API 500"),
+    Promise.resolve("API 201")
+]).then(function (results) {
+    results.forEach(function (r) {
+        let val = r.status === "fulfilled" ? r.value : r.reason;
+        console.log(r.status + " → " + val);
+    });
+});
