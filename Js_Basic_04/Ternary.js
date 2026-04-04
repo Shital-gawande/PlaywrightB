89fcc12c1480e6d270a7b138331a6819b8725a69
Shital Gawande <shitalgawande01@gmail.com>
2026-04-04 13:56:48 +0530

    modified the ternary file

--- Js_Basic_04/Ternary.js
+++ Js_Basic_04/Ternary.js
@@ -1,3 +1,11 @@
 
 let result = 5 > 3 ? "Greater" : "Smaller";
 console.log(result); // Output: "Greater"   
+
+//multiple conditions
+let age =10;
+let result2 = age>= 18 ? "Eligible ":
+              age>= 13 ? "Teenagr" :
+              age>= 10 ? "child" :
+              "Baby";
+console.log(result2); // Output: "child"
