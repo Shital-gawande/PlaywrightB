730c6f6bf48863e462c0e33d83dc0f44b3ab063e
Shital Gawande <shitalgawande01@gmail.com>
2026-04-04 15:14:08 +0530

    modified the sort array file

--- Js_Arrays_08/SortArray.js
+++ Js_Arrays_08/SortArray.js
@@ -10,3 +10,13 @@ let s1 = nums.sort((a,b)=>a-b); // ascending
 console.log(s1);
 let s2 = nums.sort((a,b)=>b-a); // descending
 console.log(s2);
+
+
+//sorting in desending order
+const fruits = ['banana', 'apple', 'cherry', 'date'];
+
+// 1. Sort ascending (A-Z) -> ['apple', 'banana', 'cherry', 'date']
+// 2. Reverse (Z-A) -> ['date', 'cherry', 'banana', 'apple']
+const descendingFruits = fruits.sort().reverse();
+
+console.log(descendingFruits);  
