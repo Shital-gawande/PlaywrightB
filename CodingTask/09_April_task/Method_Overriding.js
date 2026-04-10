3da1782bdef68898e0f5f2d0cc931beeaeec296b
Shital Gawande <shitalgawande01@gmail.com>
2026-04-10 14:51:40 +0530

    Adding tasks

---
+++ CodingTask/09_April_task/Method_Overriding.js
@@ -0,0 +1,21 @@
+class Shape {
+  area() {
+    return 0;
+  }
+}
+
+class Rectangle extends Shape {
+  constructor(w, h) {
+    super();
+    this.w = w;
+    this.h = h;
+  }
+
+  area() {
+    return this.w * this.h;
+  }
+}
+
+let r = new Rectangle(5, 3);
+console.log("Area:", r.area());
+
